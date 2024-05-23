b9c1118db1ae96a0ba57a1c2e7b9b3020dc3e9f2
JoseRGM9 <157386833+JoseRGM9@users.noreply.github.com>
2024-05-23 10:23:34 +0000

    homee

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,14 +1,17 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="container">
+		<div className="container-fluid">
 			
+				<div className="container">
+					<h1>Entrena y Transforma tu Cuerpo</h1>
+					<p>Únete a nuestra comunidad de fitness y alcanza tus objetivos.</p>
+				</div>		
 		</div>
 	);
 };
