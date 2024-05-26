da8b5614255729728e4ae52e08a0dec4ded4f0c3
JoseRGM9 <157386833+JoseRGM9@users.noreply.github.com>
2024-05-26 10:42:23 +0000

    contactanos

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
+import "../../styles/Footer.css";
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
 		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+			Copyright © Fit Titans
 		</p>
 	</footer>
 );
