ba3f03be86680484aadbf416d9715db0779a9fd4
JoseRGM9 <157386833+JoseRGM9@users.noreply.github.com>
2024-05-27 18:49:55 +0000

    cambiosjose

---
+++ src/front/js/pages/MiAreaEntrenador.jsx
@@ -0,0 +1,33 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { Context } from "../store/appContext";
+import { Link, useNavigate } from "react-router-dom";
+import "../../styles/MiAreaEntrenador.css";
+
+
+export const MiAreaEntrenador = () => {
+
+    return (
+        <div className="container">
+            <div className="tituloMiAreaEntrenador">
+                MI AREA
+            </div>
+            <div className="row row-filaMiAreaEntrenador">
+                <div className="col-md-4 columnaPerfilEntrenador">
+                    <div className="tituloPerfilRegistrado">
+                        <Link to="/" className="linkPerfilEntrenador">PERFIL</Link>
+                    </div>
+                </div>
+                <div className="col-md-4 columnaClientesDelEntrenador">
+                    <div className="tituloClientesDelEntrenador">
+                        <Link to="/" className="linkClientesDelEntrenador">CLIENTES</Link>
+                    </div>
+                </div>
+                <div className="col-md-4 columnaVideosEntrenamientoEntrenador">
+                    <div className="tituloVideosEntrenamientoEntrenador">
+                        <Link to="/" className="linkVideosEntrenamientoEntrenador">VIDEOS DE ENTRENAMIENTO</Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+};
