56b1957f76d1960bd3d9f1cd9d322a6194e37cf7
Annams02 <155559840+Annams02@users.noreply.github.com>
2024-06-05 17:06:14 +0000

    modal entrenadores, y dietas y rutinas segun admin/uer

--- src/front/js/pages/ListaEntrenadores.jsx
+++ src/front/js/pages/ListaEntrenadores.jsx
@@ -29,7 +29,8 @@ export const ListaEntrenadores = () => {
       imagen: "traerla desde cloudinary",
       edad: "23",
       genero: "xx",
-      tipoEntrenamiento: "fusufd"
+      tipoEntrenamiento: "fusufd",
+      informacion: "",
     },
     {
       nombre: "Nombre",
@@ -38,7 +39,8 @@ export const ListaEntrenadores = () => {
       imagen: "traerla desde cloudinary",
       edad: "23",
       genero: "xx",
-      tipoEntrenamiento: "fusufd"
+      tipoEntrenamiento: "fusufd",
+      informacion: "",
     },
   ]);
 
@@ -71,15 +73,13 @@ export const ListaEntrenadores = () => {
                   <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                     <div className="modal-dialog">
                       <div className="modal-content">
-                        <div className="modal-header">
-                          <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
-                          <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
+
                         <div className="modal-bodyEntrenador">
-                          ...
+                        {entrenador.informacion} aquiiii toda la infooooo del entrenador seleccionado
+                        
                         </div>
                         <div className="modal-footer">
-                          <button type="button" className="btn btn-secondaryEntrenador" data-bs-dismiss="modal">Close</button>
+                          <button type="button" className="btnCloseEntrenador" data-bs-dismiss="modal">Close</button>
                         </div>
                       </div>
                     </div>
