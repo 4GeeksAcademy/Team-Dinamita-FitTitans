190e4ec4a68823bb80dbdf8527f26886a5ea82b3
Annams02 <155559840+Annams02@users.noreply.github.com>
2024-06-14 19:02:23 +0000

    estoy retocando listadeclientes

--- src/front/js/component/PerfilUsuarios.js
+++ src/front/js/component/PerfilUsuarios.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState, useContext } from "react";
 import { useParams } from 'react-router-dom';
 import { Context } from "../store/appContext";
-import "/workspaces/Team-Dinamita-FitTitans/src/front/styles/Perfiles.css"
+
+import "../../styles/PerfilEntrenadorPrivado.css";
 
 export const PerfilUsuarios = () => {
   const [usuarios, setUsuarios] = useState([]);
@@ -88,30 +89,30 @@ fetchUsuarioUnico();
   return (
     <>
     {rol ? (
-<div className="container d-flex justify-content-center align-items-center mt-5">
-      <ul>
-        {Array.isArray(usuarios) && usuarios.map((usuario) => (
-          <li className=""key={usuario.id}>
-            <h1 className="d-flex justify-content-center">{usuario.nombre}</h1>
-            <div className="caja">
-              <div className="card">
+      <div className="container contenedorPerfil">
+        <div className="contenedorTituloPerfil">
+          <div className="form-group TituloPerfil">PERFIL DE USUARIOS</div>
+        </div>
+        <ul className="contenedorListaUsuarios">
+          {Array.isArray(usuarios) && usuarios.map((usuario) => (
+            <li key={usuario.id} className="usuarioItem">
+              <div className="card perfilCard">
                 <img src={usuario.foto} className="card-img-top" alt={`Imagen de ${usuario.nombre}`} />
+                <div className="card-body">
+                  <h5 className="card-title">{usuario.nombre}</h5>
+                </div>
                 <ul className="list-group list-group-flush">
                   <li className="list-group-item">Email: {usuario.email}</li>
                   <li className="list-group-item">Nombre: {usuario.nombre}</li>
-                  <li className="list-group-item">Telefono: {usuario.telefono}</li>
-                  <li className="list-group-item">edad: {usuario.edad}</li>
-                  <li className="list-group-item">Genero: {usuario.genero}</li>
+                  <li className="list-group-item">Teléfono: {usuario.telefono}</li>
+                  <li className="list-group-item">Edad: {usuario.edad}</li>
+                  <li className="list-group-item">Género: {usuario.genero}</li>
                   <li className="list-group-item">Altura: {usuario.altura}</li>
                 </ul>
               </div>
-              <label htmlFor={`file-input-${usuario.id}`}>
-                Subir Imagen &#11015;
-              </label>
               <input
                 type="file"
                 accept="image/*"
-                placeholder="cambiar o subir imagen"
                 onChange={(e) => handleSubirImagen(usuario.id, e.target.files[0])}
               />
               {editar ? (
@@ -136,7 +137,7 @@ fetchUsuarioUnico();
                     type="text"
                     name="telefono"
                     className="form-control"
-                    placeholder="Telefono"
+                    placeholder="Teléfono"
                     onChange={manejarCambio}
                     defaultValue={usuario.telefono}
                   />
@@ -167,19 +168,18 @@ fetchUsuarioUnico();
                     onChange={manejarCambio}
                     defaultValue={usuario.altura}
                   />
-                  <button onClick={() => manejarEditarUsuario(usuario.id)}>Guardar</button>
+                  <button className="subirEditar" onClick={() => manejarEditarUsuario(usuario.id)}>Guardar</button>
                 </>
               ) : (
-                <button onClick={() => setEditar(true)}>Editar</button>
+                <button className="subirEditar" onClick={() => setEditar(true)}>Editar</button>
               )}
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
-    ) : (<h1> ERROR </h1>)}
-    
-    </>
-    
-  );
+            </li>
+          ))}
+        </ul>
+      </div>
+    ) : (
+      <h1> ERROR </h1>
+    )}
+  </>
+);
 };
