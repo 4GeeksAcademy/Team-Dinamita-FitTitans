b3f1a2576f26e1c4c7442ff5f198934c12caa0b1
Annams02 <155559840+Annams02@users.noreply.github.com>
2024-06-21 16:38:30 +0000

    perfiles dieta rutina chat responsive

--- src/front/js/pages/VideosUsuario.jsx
+++ src/front/js/pages/VideosUsuario.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import { Context } from '../store/appContext.js';
-import "../../styles/VideosEntrenador.css"
+import "../../styles/VideosUsuario.css"
 
 export const VideosUsuarios = () => {
   const { store, actions } = useContext(Context);
